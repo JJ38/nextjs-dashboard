ee8047f8e5cbe963951753ddd8f1f9d2320d97e3
JJ38 <jamesbrass@ymail.com>
2024-12-26 21:32:01 +0000

    added new routes

--- app/ui/invoices/status.tsx
+++ app/ui/invoices/status.tsx
@@ -4,10 +4,10 @@ import clsx from 'clsx';
 export default function InvoiceStatus({ status }: { status: string }) {
   return (
     <span
-      className={clsx(
+      className={clsx( //clsx allows the toggling of class names
         'inline-flex items-center rounded-full px-2 py-1 text-xs',
         {
-          'bg-gray-100 text-gray-500': status === 'pending',
+          'bg-gray-100 text-gray-500': status === 'pending', 
           'bg-green-500 text-white': status === 'paid',
         },
       )}
